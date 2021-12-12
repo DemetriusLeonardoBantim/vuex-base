c53ca70689af200d07fb099b86a14a8869902940
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2021-12-12 10:14:33 -0300

    anotacoes importantes

--- src/store.js
+++ src/store.js
@@ -19,3 +19,65 @@ export default new Vuex.Store({
     }
   }
 })
+
+// Actions: Quando precisamos de uma lógica ligada a alteração do estado ou caso exista a necessidade de dispararmos mais de uma mutaçao
+// utilizamos actions. Actions também permitem eventos assíncronos, diferente de mutaions
+
+/**
+ *  Exemplo de action + state + mutation
+ *  state:{
+ *    name: "Lobo",
+ *    id: "2435"
+ *  }
+ *  mutations: {
+ *    CHANGE_USER(state , payload){
+ *      state.name = payload.name
+ *    }
+ *    CHANGE_ID(state, payload){
+ *      state.id = payload.id
+ *    } 
+ *   }
+ *  actions:{
+ *   changeUser(context,payload){
+ *      context.commit('CHANGE_USER', payload)
+ *      context.commit('CHANGE_ID', payload) 
+ *    }
+ *  }
+  Actions podem ser utilizadas para códigos assincronos.
+ */
+
+  // Getters são como as computed properties dos componentes. Deve ser utilizados quando precisamos fazer algo programático com o estado.
+  /**
+   *  Exemplo de getters
+   *  state: {
+   *    livros: [
+   *      { nome: Senhor do aneis, lido: true}
+   *      {nome: Harry Poder, lido: false}
+   *      {nome : Breaking Bad, lido: true}
+   *    ]
+   *  }
+   *  getters:{
+   *    livrosLidos(state){
+   *      return state.livros.filter(livro => livro.lido)
+   *    }
+   *  }
+   * 
+   * 
+   *  */
+
+    // Quando seu aplicativo começa a crescer e ficar mais complexo, o ideal é separar o código em diferente partes. Para isso podemos utilizar modules
+    /**
+     *  exemplo de utilização:
+     *  import login from '@/store/login.js'
+     *  import cart from '@/store/cart.js
+     *  
+     *  export defualt new Vuex.store({
+     *  modules: {
+     *    login,
+     *    cart
+     *  }
+     * 
+     * })
+     *  
+     */
+      //Sendo assim, para usar as informações presentes no state do login, devemos fazer: $store.state.login.xxx
